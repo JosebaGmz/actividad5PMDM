44d407575f0f7460ee3b0467745522cf40df7e53
JosebaGmz <josebagomezcaballero90@gmail.com>
2024-03-05 17:34:33 +0100

    actividad 4 y 5 realizada botones implementados y string pasado a la activity2 para que muestre el texto debajo del mensaje de bienvenida

--- app/src/main/java/com/example/actividad5pmdm/Activity2.kt
+++ app/src/main/java/com/example/actividad5pmdm/Activity2.kt
@@ -2,10 +2,22 @@ package com.example.actividad5pmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Typeface
+import android.widget.TextView
 
 class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        val typeface = Typeface.createFromAsset(assets, "fonts/comic.ttf")
+        val msg = intent.getStringExtra("message")
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val textViewMessage = findViewById<TextView>(R.id.textViewMessage)
+
+        textViewMessage.text = msg
+        textView.typeface = typeface
+        textViewMessage.typeface = typeface
     }
 }
--- app/src/main/java/com/example/actividad5pmdm/MainActivity.kt
+++ app/src/main/java/com/example/actividad5pmdm/MainActivity.kt
@@ -3,19 +3,38 @@ package com.example.actividad5pmdm
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.FrameLayout
+import android.widget.EditText
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bodyContainer: FrameLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bodyContainer = findViewById(R.id.bodyContainer)
-        setBodyBackgroundColor(bodyContainer, Color.BLUE)
-    }
+        val buttonChangeColor = findViewById<Button>(R.id.buttonChangeColor)
+        val buttonSwitchActivity = findViewById<Button>(R.id.buttonSwitchActivity)
 
-    private fun setBodyBackgroundColor(container: FrameLayout, color: Int) {
-        container.setBackgroundColor(color)
+        buttonChangeColor.setOnClickListener {
+            // Cambiar el color de fondo del cuerpo
+            val newColor = Color.rgb(0, 0, 255)
+            val container = findViewById<LinearLayout>(R.id.container)
+            container.setBackgroundColor(newColor)
+        }
+
+        buttonSwitchActivity.setOnClickListener {
+            // Cambiar a Activity 2
+            val editTextMessage = findViewById<EditText>(R.id.editText)
+            val message = editTextMessage.text.toString()
+
+            val intent = Intent(this, Activity2::class.java).apply {
+                putExtra("message", message)
+            }
+            startActivity(intent)
+        }
+
+
     }
 }
