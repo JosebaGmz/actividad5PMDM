1172fd1db096cdd2323cd038da954c867376a367
JosebaGmz <josebagomezcaballero90@gmail.com>
2024-03-05 16:38:11 +0100

    Recuperado todo el contenido de mi anterior proyecto corrupto

--- app/src/main/java/com/example/actividad5pmdm/MainActivity.kt
+++ app/src/main/java/com/example/actividad5pmdm/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.actividad5pmdm
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bodyContainer: FrameLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bodyContainer = findViewById(R.id.bodyContainer)
+        setBodyBackgroundColor(bodyContainer, Color.BLUE)
+    }
+
+    private fun setBodyBackgroundColor(container: FrameLayout, color: Int) {
+        container.setBackgroundColor(color)
     }
 }
